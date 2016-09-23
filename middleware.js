f3bfcc4138077dd544e05c633277b9434d4c1395
Sireesha Muppala <sireesha.muppala@bhnetwork.com>
2016-09-23 15:19:58 -0600

    Refactored middleware to a seperate file

---
+++ middleware.js
@@ -0,0 +1,16 @@
+/**
+ * Created by smupp00 on 9/23/16.
+ */
+
+var middleware = {
+    requireAuthentication : function(req, res, next) {
+        console.log('Private route hit');
+        next();
+    },
+    logger : function(req, res, next) {
+        console.log('Request : ' +  new Date() + ' ' + req.method + ' ' + req.originalUrl)
+        next();
+    }
+}
+
+module.exports = middleware;
