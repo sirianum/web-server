873ba54bb8c1645ea29939a0708152ed669c3cec
Sireesha Muppala <sireesha.muppala@bhnetwork.com>
2016-09-23 15:11:45 -0600

    Initialize node js repo

---
+++ server.js
@@ -0,0 +1,32 @@
+/**
+ * Created by smupp00 on 9/23/16.
+ */
+var express = require('express')
+var app = express();
+var PORT = 3000
+
+var middleware = {
+    requireAuthentication : function(req, res, next) {
+        console.log('Private route hit');
+        next();
+    },
+    logger : function(req, res, next) {
+        console.log('Request : ' +  new Date() + ' ' + req.method + ' ' + req.originalUrl)
+        next();
+    }
+}
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+    res.send('About Us!!')
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function(){
+    console.log("Express server started and listening at " + PORT + "!!!!")
+});
+
