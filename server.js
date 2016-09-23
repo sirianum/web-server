568b824195477f26cff306cfbe3785f69b55c8ef
Sireesha Muppala <sireesha.muppala@bhnetwork.com>
2016-09-23 15:19:42 -0600

    Refactored middleware to a seperate file

--- server.js
+++ server.js
@@ -5,16 +5,7 @@ var express = require('express')
 var app = express();
 var PORT = 3000
 
-var middleware = {
-    requireAuthentication : function(req, res, next) {
-        console.log('Private route hit');
-        next();
-    },
-    logger : function(req, res, next) {
-        console.log('Request : ' +  new Date() + ' ' + req.method + ' ' + req.originalUrl)
-        next();
-    }
-}
+var middleware = require('./middleware.js')
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
